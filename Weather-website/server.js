6d0924636fd0f6623692c03f241738de81871b5d
Sachin Kumar <sac9798@gmail.com>
2019-10-11 20:59:38 +0530

    openweather API

---
+++ Weather-website/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiKey = ''
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } 
+    
+    else {
+      let weather = JSON.parse(body)
+      
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } 
+      
+      else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}! with ${weather.weather[0]["main"]}`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
